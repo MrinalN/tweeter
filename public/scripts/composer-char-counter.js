90c4f8ccca2a183508ccd2e06c81aef69e71ab52
Adele Noronha <adele.c.noronha@gmail.com>
2021-03-26 18:46:55 +0000

    counter changes color on negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,15 @@
+//Function counts length of input text. Reduces counter and changes color accordingly
+
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
    let textSize = $(this).val().length;
-  $(".counter").val(140 - textSize); 
-
-  
+   let maxNum = 140;
+  $(".counter").val(maxNum - textSize); 
+   if (textSize > maxNum) {
+    $(".counter").css({'color': 'red'})
+   } else {
+    $(".counter").css({'color': 'black'})
+   }
   });
 });
 
-//counter
